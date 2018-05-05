52c30e7e5990f6d8bee32f29e695431e03573cea
JerrysTian <1439478728@qq.com>
2018-05-05 11:15:35 +0800

    controller层文件格式修改

--- src/main/java/com/sdll/blog/controller/BlogNoteController.java
+++ src/main/java/com/sdll/blog/controller/BlogNoteController.java
@@ -43,24 +43,23 @@ import freemarker.template.TemplateException;
 
 @Controller
 public class BlogNoteController {
-	
-	/**项目所在tomcat位置*/
+
+	/** 项目所在tomcat位置 */
 	public String appRootPath;
-	
+
 	public String handledBase64Block;
-	
+
 	public String xmlimaHref;
-	
+
 	@Autowired
 	IBlogNoteService blogNoteService;
-	
+
 	@RequestMapping("ajax/blogList")
 	@ResponseBody
 	public List<BlogNote> blogList() {
-		
 		return null;
 	}
-	
+
 	@RequestMapping("ajax/addOrEditBlog")
 	@ResponseBody
 	public ExecResult addOrEditBlog(BlogNote blogNote) {
@@ -70,39 +69,40 @@ public class BlogNoteController {
 		er.setSuccess(flag != 0 ? true : false);
 		return er;
 	}
-	
+
 	@RequestMapping("ajax/upLoad")
 	@ResponseBody
-	public Map<String,Object> upLoad(HttpServletRequest request, HttpServletResponse response) {
-		//创建一个通用的多部分解析器  
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());  
-        Map<String,Object> map = new HashMap<String,Object>();
-        //判断 request 是否有文件上传,即多部分请求  
-        if(multipartResolver.isMultipart(request)){  
-            //转换成多部分request    
-            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest)request;  
-            //取得request中的所有文件名  
-            Iterator<String> iter = multiRequest.getFileNames();  
-            List<String> data = new ArrayList<String>();
-            while(iter.hasNext()){ 
-                //取得上传文件  
-                MultipartFile file = multiRequest.getFile(iter.next());  
-                if(file != null){  
-                    //取得当前上传文件的文件名称  
-                    String myFileName = file.getOriginalFilename();  
-                    //如果名称不为“”,说明该文件存在，否则说明该文件不存在  
-                    if(myFileName.trim() != "") {  
-                        //System.out.println(myFileName);  
-                        //重命名上传后的文件名  
-                        String fileName = "/../upload/" + file.getOriginalFilename();  
-                        //定义上传路径  
-                        String appRoot = request.getSession().getServletContext().getRealPath("") + File.separator;
-                        String path =  appRoot + fileName;  
-                        File localFile = new File(path);
-                        if(!localFile.exists()) {
-                        	localFile.mkdirs();
-                        }
-                        try {
+	public Map<String, Object> upLoad(HttpServletRequest request, HttpServletResponse response) {
+		// 创建一个通用的多部分解析器
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+				request.getSession().getServletContext());
+		Map<String, Object> map = new HashMap<String, Object>();
+		// 判断 request 是否有文件上传,即多部分请求
+		if (multipartResolver.isMultipart(request)) {
+			// 转换成多部分request
+			MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+			// 取得request中的所有文件名
+			Iterator<String> iter = multiRequest.getFileNames();
+			List<String> data = new ArrayList<String>();
+			while (iter.hasNext()) {
+				// 取得上传文件
+				MultipartFile file = multiRequest.getFile(iter.next());
+				if (file != null) {
+					// 取得当前上传文件的文件名称
+					String myFileName = file.getOriginalFilename();
+					// 如果名称不为“”,说明该文件存在，否则说明该文件不存在
+					if (myFileName.trim() != "") {
+						// System.out.println(myFileName);
+						// 重命名上传后的文件名
+						String fileName = "/../upload/" + file.getOriginalFilename();
+						// 定义上传路径
+						String appRoot = request.getSession().getServletContext().getRealPath("") + File.separator;
+						String path = appRoot + fileName;
+						File localFile = new File(path);
+						if (!localFile.exists()) {
+							localFile.mkdirs();
+						}
+						try {
 							file.transferTo(localFile);
 							data.add(fileName);
 						} catch (Exception e) {
@@ -110,17 +110,16 @@ public class BlogNoteController {
 							map.put("errno", 1);
 							map.put("data", data);
 							return map;
-						}  
-                    }  
-                }  
-            }  
-            map.put("data", data);
-            map.put("errno", 0);
-        }  
+						}
+					}
+				}
+			}
+			map.put("data", data);
+			map.put("errno", 0);
+		}
 		return map;
 	}
-	
-	
+
 	@RequestMapping("/blog/view")
 	public ModelAndView view(String id) {
 		ModelAndView view = new ModelAndView("plugins/blog/view");
@@ -134,26 +133,29 @@ public class BlogNoteController {
 		view.addObject("downBlogNote", downBlogNote);
 		return view;
 	}
-	
+
 	@RequestMapping("/blog/ajax/downloadBlog")
-	public void downloadBlog(String blogIds, HttpServletResponse response, HttpServletRequest request) throws IOException {
+	public void downloadBlog(String blogIds, HttpServletResponse response, HttpServletRequest request)
+			throws IOException {
 		String[] blogIdArr = blogIds.split(",");
-		appRootPath = request.getSession().getServletContext().getRealPath("/");  //寻找试题图片的时候需要这个
+		appRootPath = request.getSession().getServletContext().getRealPath("/"); // 寻找试题图片的时候需要这个
 		if (blogIdArr != null && blogIdArr.length == 1) {
-			//单个博客文章下载     
+			// 单个博客文章下载
 			handledBase64Block = "";
 			xmlimaHref = "";
 			BlogNote blogNote = blogNoteService.getByPrimaryKey(blogIdArr[0]);
 			// 设置response参数，可以打开下载页面
-			String fileName = blogNote.getTitle() + "_" + DateUtils.getDateToStringByPattern(new Date(), "yyyyMMddHHmmss");
-	        response.reset();
-	        response.setContentType("application/msword;charset=utf-8");
-	        response.setHeader("Content-Disposition", "attachment;filename="+ new String(fileName.getBytes("GBK"), "ISO8859-1") + ".doc");
-	        ServletOutputStream out = response.getOutputStream();
+			String fileName = blogNote.getTitle() + "_"
+					+ DateUtils.getDateToStringByPattern(new Date(), "yyyyMMddHHmmss");
+			response.reset();
+			response.setContentType("application/msword;charset=utf-8");
+			response.setHeader("Content-Disposition",
+					"attachment;filename=" + new String(fileName.getBytes("GBK"), "ISO8859-1") + ".doc");
+			ServletOutputStream out = response.getOutputStream();
 			downloadSingleBlog(out, blogNote, response);
 		}
 		if (blogIdArr != null && blogIdArr.length != 1) {
-			//多篇博客文章下载
+			// 多篇博客文章下载
 			String zipName = "博客下载.zip" + "_" + DateUtils.getDateToStringByPattern(new Date(), "yyyyMMddHHmmss");
 			response.setCharacterEncoding("ISO8859-1");
 			response.setContentType("APPLICATION/OCTET-STREAM"); // 设置response的编码方式
@@ -162,12 +164,13 @@ public class BlogNoteController {
 			ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(response.getOutputStream()));
 			BlogNote blogNote = null;
 			for (String id : blogIdArr) {
-				//单个博客文章下载   
+				// 单个博客文章下载
 				handledBase64Block = "";
 				xmlimaHref = "";
 				blogNote = blogNoteService.getByPrimaryKey(id);
 				// 设置response参数，可以打开下载页面
-				String fileName = blogNote.getTitle() + "_" + DateUtils.getDateToStringByPattern(new Date(), "yyyyMMddHHmmss");
+				String fileName = blogNote.getTitle() + "_"
+						+ DateUtils.getDateToStringByPattern(new Date(), "yyyyMMddHHmmss");
 				out.putNextEntry(new ZipEntry(fileName + ".doc"));
 				ByteArrayOutputStream os = new ByteArrayOutputStream();
 				downloadSingleBlog(os, blogNote, response);
@@ -183,57 +186,64 @@ public class BlogNoteController {
 			}
 		}
 	}
-	
+
 	/**
 	 * 下载单篇或多篇博客文章提取的公共方法
+	 * 
 	 * @createUser shandianlala
 	 * @createDate 2017年10月20日
-	 * @param out 输出流
-	 * @param blogNote 博客文章
-	 * @param response http响应
+	 * @param out
+	 *            输出流
+	 * @param blogNote
+	 *            博客文章
+	 * @param response
+	 *            http响应
 	 * @throws IOException
 	 */
-	private void downloadSingleBlog(OutputStream out,  BlogNote blogNote, HttpServletResponse response) throws IOException{
+	private void downloadSingleBlog(OutputStream out, BlogNote blogNote, HttpServletResponse response)
+			throws IOException {
 		Template template = getTemplate();
-		if(template != null) {
-	        Map<String, Object> dataMap = new HashMap<String, Object>();
-			dataMap.put("createTime", DateUtils.getDateToStringByPattern(blogNote.getCreateTime(), "YYYY年MM月dd日  hh:mm"));
+		if (template != null) {
+			Map<String, Object> dataMap = new HashMap<String, Object>();
+			dataMap.put("createTime",
+					DateUtils.getDateToStringByPattern(blogNote.getCreateTime(), "YYYY年MM月dd日  hh:mm"));
 			RichHtmlHandler handler = new RichHtmlHandler(blogNote.getBlogContent(), appRootPath + File.separator);
 			blogNote.setBlogContent(handler.getHandledDocBodyBlock());
 			handledBase64Block += handler.getData(handler.getDocBase64BlockResults());
 			xmlimaHref += handler.getData(handler.getXmlImgRefs());
 			dataMap.put("imagesBase64String", handledBase64Block);
-	        dataMap.put("imagesXmlHrefString", xmlimaHref);
-	        dataMap.put("blogNote", blogNote);
-	        Writer wb = null;
+			dataMap.put("imagesXmlHrefString", xmlimaHref);
+			dataMap.put("blogNote", blogNote);
+			Writer wb = null;
 			try {
 				wb = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
-				template.process(dataMap, wb);//写数据到模板
+				template.process(dataMap, wb);// 写数据到模板
 				wb.close();
 			} catch (FileNotFoundException e1) {
 				e1.printStackTrace();
 			} catch (TemplateException e) {
 				e.printStackTrace();
 			}
-	        
+
 		}
 	}
-	
+
 	/**
 	 * 获取博客文章下载模板 .ftl文件
+	 * 
 	 * @createUser shandianlala
 	 * @createDate 2017年10月20日
 	 * @return
 	 */
 	public Template getTemplate() {
-		//设置模本装置方法和路径,FreeMarker支持多种模板装载方法。可以重servlet，classpath，数据库装载，
-		//这里我们的模板是放在resources/template 目录下面
+		// 设置模本装置方法和路径,FreeMarker支持多种模板装载方法。可以重servlet，classpath，数据库装载，
+		// 这里我们的模板是放在resources/template 目录下面
 		Configuration configuration = new Configuration();
 		configuration.setDefaultEncoding("UTF-8");
 		configuration.setClassForTemplateLoading(this.getClass(), "/");
 		Template template = null;
 		try {
-			//blogTemplate.ftl为要装载的模板
+			// blogTemplate.ftl为要装载的模板
 			template = configuration.getTemplate("template/blogTemplate.ftl", "UTF-8");
 			return template;
 		} catch (IOException e) {
@@ -241,6 +251,5 @@ public class BlogNoteController {
 		}
 		return null;
 	}
-	
 
 }
--- src/main/java/com/sdll/blog/controller/MainController.java
+++ src/main/java/com/sdll/blog/controller/MainController.java
@@ -20,18 +20,18 @@ public class MainController {
 
 	@Autowired
 	IBlogUserService userService;
-	
+
 	@Autowired
 	IDictionaryService dictionaryService;
-	
+
 	@Autowired
 	IBlogNoteService blogNoteService;
 
 	@RequestMapping("/main/toHome")
-	public ModelAndView toIndex(){
+	public ModelAndView toIndex() {
 		ModelAndView view = new ModelAndView("plugins/home/home");
-//		List<Menu> menuList = menuService.listMenu();
-//		InformationTool.getInformation(view, menuList);
+		// List<Menu> menuList = menuService.listMenu();
+		// InformationTool.getInformation(view, menuList);
 		return view;
 	}
 
@@ -44,7 +44,7 @@ public class MainController {
 		view.addObject("blogNote", blogNote);
 		return view;
 	}
-	
+
 	@RequestMapping("/backStage")
 	public ModelAndView backStage() {
 		ModelAndView view = new ModelAndView(getBasePath() + "menuList");
@@ -52,66 +52,66 @@ public class MainController {
 		view.addObject("dictionaryList", dictionaryList);
 		return view;
 	}
-	
+
 	private String getBasePath() {
 		return "plugins/backstage/";
 	}
 
-//
-//	@RequestMapping("/validate")
-//	public void validate(HttpSession session , HttpServletRequest request , HttpServletResponse response) throws IOException{
-//		String key = RandomValidateCodeGenerator.randKey(4);
-//		try {
-//			Map.Entry<String, BufferedImage> randCode = new RandomValidateCodeGenerator().getRandCode(key);
-//			session.setAttribute(Conts.VALIDATE_CODE_KEY, randCode.getKey());
-//			ImageIO.write(randCode.getValue(), "JPEG", response.getOutputStream());
-//		} catch (Exception e) {
-//			System.out.println("生成验证码图片失败了!");
-//			e.printStackTrace();
-//		}
-//	}
-//
-//	@RequestMapping("/userLogin")
-//	@ResponseBody
-//	public ExecResult userLogin(User user , HttpSession session){
-//		ExecResult er = new ExecResult();
-//		User usertemp = null;
-//		String validateCode = (String) session.getAttribute(Conts.VALIDATE_CODE_KEY);
-//		if (!user.getValidateCode().equalsIgnoreCase(validateCode)) {
-//			er.setMsg("验证码错误！");
-//			return er;
-//		}
-//		if (!StringUtils.isNotBlank(user.getUserPhone())) {
-//			er.setMsg("登陆失败，请刷新页面试试");
-//			return er;
-//		}
-//		usertemp = userService.userLogin(user.getUserPhone());
-//		if (usertemp == null) {
-//			er.setMsg("账户不存在！");
-//			return er;
-//		}
-//		if (!user.getUserPassword().equals(usertemp.getUserPassword())) {
-//			er.setMsg("账号或者密码错误！");
-//			return er;
-//		}
-//		session.setAttribute(Conts.USER_SESSION_KEY, usertemp);
-//		er.setSuccess(true);
-//		er.setMsg("登陆成功");
-//		return er;
-//	}
-//
-//	@RequestMapping("/logOut")
-//	public ModelAndView logOut(HttpSession session){
-//		session.invalidate();
-//		ModelAndView view = new ModelAndView("/index");
-//		List<Menu> menuList = menuService.listMenu();
-//		InformationTool.getInformation(view, menuList);
-//		return view;
-//	}
-//
-//
-//
-	
-	
+	//
+	// @RequestMapping("/validate")
+	// public void validate(HttpSession session , HttpServletRequest request ,
+	// HttpServletResponse response) throws IOException{
+	// String key = RandomValidateCodeGenerator.randKey(4);
+	// try {
+	// Map.Entry<String, BufferedImage> randCode = new
+	// RandomValidateCodeGenerator().getRandCode(key);
+	// session.setAttribute(Conts.VALIDATE_CODE_KEY, randCode.getKey());
+	// ImageIO.write(randCode.getValue(), "JPEG", response.getOutputStream());
+	// } catch (Exception e) {
+	// System.out.println("生成验证码图片失败了!");
+	// e.printStackTrace();
+	// }
+	// }
+	//
+	// @RequestMapping("/userLogin")
+	// @ResponseBody
+	// public ExecResult userLogin(User user , HttpSession session){
+	// ExecResult er = new ExecResult();
+	// User usertemp = null;
+	// String validateCode = (String) session.getAttribute(Conts.VALIDATE_CODE_KEY);
+	// if (!user.getValidateCode().equalsIgnoreCase(validateCode)) {
+	// er.setMsg("验证码错误！");
+	// return er;
+	// }
+	// if (!StringUtils.isNotBlank(user.getUserPhone())) {
+	// er.setMsg("登陆失败，请刷新页面试试");
+	// return er;
+	// }
+	// usertemp = userService.userLogin(user.getUserPhone());
+	// if (usertemp == null) {
+	// er.setMsg("账户不存在！");
+	// return er;
+	// }
+	// if (!user.getUserPassword().equals(usertemp.getUserPassword())) {
+	// er.setMsg("账号或者密码错误！");
+	// return er;
+	// }
+	// session.setAttribute(Conts.USER_SESSION_KEY, usertemp);
+	// er.setSuccess(true);
+	// er.setMsg("登陆成功");
+	// return er;
+	// }
+	//
+	// @RequestMapping("/logOut")
+	// public ModelAndView logOut(HttpSession session){
+	// session.invalidate();
+	// ModelAndView view = new ModelAndView("/index");
+	// List<Menu> menuList = menuService.listMenu();
+	// InformationTool.getInformation(view, menuList);
+	// return view;
+	// }
+	//
+	//
+	//
 
 }
